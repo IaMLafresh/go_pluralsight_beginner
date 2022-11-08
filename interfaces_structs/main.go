28e92172247b1ea4f9ca3257d0347c6092898202
IaMLafresh <mglabreche@gmail.com>
2022-11-08 12:58:40 -0500

    Final code input to Github

--- interfaces_structs/main.go
+++ interfaces_structs/main.go
@@ -1,5 +1,22 @@
+/*
+This course talked about creating structs and interfaces in a separate class/package
+It also discussed the importance of using pointer based objects for setting and receiving variables
+*/
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/go_pluralsight_beginner/interfaces_structs/organization"
+)
+
+func main() {
+	p := organization.NewPerson("Mike", "LaBreche")
+	err := p.SetTwitterHandler("@IAM")
+	if err != nil {
+		fmt.Printf("An error occurred setting Twitter handler: %s\n", err.Error())
+	}
+	println(p.GetTwitterHandler())
+	println(p.ID())
+	println(p.FullName())
 }
