44a6cec76f0bffb7a32ae3bd7b7dc3f8139c1f3f
IaMLafresh <mglabreche@gmail.com>
2022-11-08 13:10:08 -0500

    New code for Type Def

---
+++ typeDefinitions_Aliases/organization/person.go
@@ -0,0 +1,49 @@
+package organization
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type TwitterHandler = string
+
+type Idenfifiable interface {
+	ID() string
+}
+
+type Person struct {
+	firstName      string
+	lastName       string
+	twitterHandler TwitterHandler
+}
+
+func (p Person) ID() string {
+	return "12345"
+}
+
+func (p Person) FullName() string {
+	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
+}
+
+func NewPerson(firstName, lastName string) Person {
+	return Person{
+		firstName: firstName,
+		lastName:  lastName,
+	}
+}
+
+func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
+	if len(handler) == 0 {
+		p.twitterHandler = handler
+	} else if !strings.HasPrefix(handler, "@") {
+		return errors.New("Twitter handler must start with an @ symbol")
+	}
+
+	p.twitterHandler = handler
+	return nil
+}
+
+func (p Person) GetTwitterHandler() string {
+	return p.twitterHandler
+}
